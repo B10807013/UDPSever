1d4aa892638b0715ff3f32f5519d3accf12b8997
B10807013 <object10422@gmail.com>
2019-10-17 14:46:32 +0800

    practice of UDPServer (origin)

---
+++ src/udpServer/exercise/Main.java
@@ -0,0 +1,28 @@
+package udpServer.exercise;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class Main {
+    public static void main(String[] args){
+        final int PORT = 8000;
+
+        try{
+            DatagramSocket ds = new DatagramSocket(PORT);
+            byte[] buf = new byte[1024];
+            DatagramPacket dp = new DatagramPacket(buf, buf.length);
+
+            while(true){
+                ds.receive(dp);
+                String s = new String(dp.getData(), 0, dp.getLength());
+                System.out.println("receive: " + s);
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
